ea1ae7cea623720b4a64c1e91580b3bc64be9593
Raj Gopal <harirajagopal393@gmail.com>
2020-04-16 00:01:11 +0530

    Added feature to know coin status when flipped

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,2 +1,12 @@
-#! /bin/bash
-echo "this project is about coin flip simulation"
+#!/bin/bash -x
+
+flipCheck=$((RANDOM%2))
+isHead=1
+isTail=0
+
+if [ $check -eq $Heads ]
+then
+    echo "This time Head"
+else
+    echo "This time Tail"
+fi
