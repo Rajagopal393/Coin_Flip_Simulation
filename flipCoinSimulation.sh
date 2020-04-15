c0ccbaa1135e1ca37827cfa5b6cc6216e4cec4a8
Raj Gopal <harirajagopal393@gmail.com>
2020-04-16 01:15:11 +0530

    Added feature for flipping a coin as per user given times

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,12 +1,26 @@
 #!/bin/bash -x
 
-flipCheck=$((RANDOM%2))
+echo "enter number of times u want to flip the coin"
+read numberOfFlips
+
 isHead=1
 isTail=0
+headsCount=0
+tailsCount=0
 
-if [ $check -eq $Heads ]
-then
-    echo "This time Head"
-else
-    echo "This time Tail"
-fi
+for (( count=1; $count<=$numberOfFlips; count++ ))
+	do
+	    flipCheck=$((RANDOM%2))
+	    if [ $isHead -eq $flipCheck ]
+	       then
+            	   headsCount=$(($headsCount+1))
+	       else
+    	    	   tailsCount=$(($tailsCount+1))
+	    fi
+	done
+
+headPercentage=$(( $headsCount*100/$numberOfFlips ))
+tailPercentage=$(( $tailsCount*100/$numberOfFlips ))
+
+echo "Percentage of Head: $headPercentage and Percentage of Tail: $tailPercentage"
+
