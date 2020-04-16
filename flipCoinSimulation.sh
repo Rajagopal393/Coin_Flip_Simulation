c7979fd933cf28ce0f0023b7e74d00ad1f581770
Rajagopal393 <harirajagopal393@gmail.com>
2020-04-16 20:32:39 +0530

    Added feature to use two coins

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -7,20 +7,39 @@ isHead=1
 isTail=0
 headsCount=0
 tailsCount=0
+numberOfCoins=2
+sum=$numberOfCoins
+Head=0
+Tail=0
 
-for (( count=1; $count<=$numberOfFlips; count++ ))
+for ((  count=1; $count<=$numberOfFlips; count++ ))
 	do
-	    flipCheck=$((RANDOM%2))
-	    if [ $isHead -eq $flipCheck ]
-	       then
-            	   headsCount=$(($headsCount+1))
-	       else
-    	    	   tailsCount=$(($tailsCount+1))
-	    fi
+		headsCount=0 
+		for (( coinCount=1; $coinCount<=$numberOfCoins; coinCount++ ))
+			do
+	    			flipCheck=$((RANDOM%2))
+	   			if [ $isHead -eq $flipCheck ]; then
+            	   				headsCount=$(($headsCount+1))
+	       			else
+    	    	   				tailsCount=$(($tailsCount+0))
+	    			fi
+			done
+		if [ $headsCount -eq $numberOfCoins ]; then
+			Head=$(($Head+1))
+		elif [ $sum -eq 0 ]; then
+			Tail=$(($Tail+1))
+		fi
 	done
 
-headPercentage=$(( $headsCount*100/$numberOfFlips ))
-tailPercentage=$(( $tailsCount*100/$numberOfFlips ))
 
-echo "Percentage of Head: $headPercentage and Percentage of Tail: $tailPercentage"
+headPercentage=$(($Head*100/$numberOfFlips))
+echo "Head Percentage: $headPercentage"
+tailPercentage=$((100-$headPercentage))
+echo "Tail Percentage: $tailPercentage"
 
+
+if [ $headPercentage -gt $tailPercentage ]; then
+	echo "Most combination is Head"
+else
+	echo "Most combination is Tail"
+fi
